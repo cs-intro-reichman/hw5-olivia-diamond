9d1edd1db5258a259edef015405e7204906ca7c4
Olivia Diamond <oliviadiamond@Olivias-MacBook-Air-2.local>
2024-12-06 18:23:40 +0200

    2nd version

--- MyString.java
+++ MyString.java
@@ -78,7 +78,7 @@ public class MyString {
      */
     public static String spacedString(String str) {
         StringBuilder sb = new StringBuilder(str);
-            for(int i = -1; i<(sb.length() -1); i+=2) {
+            for(int i = 0; i<(sb.length() -1); i+=2) {
                 sb.insert(i+1, " ");
             }
 
